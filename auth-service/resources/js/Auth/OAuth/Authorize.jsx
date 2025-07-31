381c3a68f194e3464f670ebd2d8879502b5abf4a
Christian24GH <loquezchristian@gmail.com>
2025-07-31 11:33:15 +0800

    clean

--- auth-service/resources/js/Auth/OAuth/Authorize.jsx
+++
@@ -1,6 +0,0 @@
-import { Head } from "@inertiajs/react";
-export default function Authorize(){
-    return <>
-        <Head title="Authorize"/>
-    </>
-}
--- auth-service/routes/web.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-use App\Http\Controllers\API\PageController;
-use Illuminate\Support\Facades\Route;
-
-
-Route::get('/login', [PageController::class, 'login'])->name('login');
-Route::get('/register', [PageController::class, 'register'])->name('register');
-
