b40bfa8649d5283813cc5933e48088dba9fef319
Christian24GH <loquezchristian@gmail.com>
2025-09-03 15:40:12 +0800

    frontend

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -2,6 +2,7 @@ import axios from 'axios'
 
 export default axios.create({
   baseURL: "https://auth-backend-v20b.onrender.com", // auth backend on port 8091
+  //baseURL: "http://localhost:8091/", // auth backend on port 8091
   withCredentials: true, // important for Sanctum
   withXSRFToken: true,
 });
