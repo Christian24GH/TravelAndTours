9b6c584bf7eda349c7947bce84d4a666910ffa4c
Christian24GH <loquezchristian@gmail.com>
2025-09-03 17:38:50 +0800

    fixed cors/sanctum

--- auth/config/cors.php
+++ auth/config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['*'],
+    'allowed_origins' => ['https://travelandtours-c9xk.onrender.com','*'],
 
     'allowed_origins_patterns' => [],
 
