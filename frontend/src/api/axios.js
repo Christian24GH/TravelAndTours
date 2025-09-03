cfda1c8b25be15508638cc3a65ad39ebb0eeeee1
Christian24GH <loquezchristian@gmail.com>
2025-09-03 23:38:00 +0800

    frontend

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -5,4 +5,5 @@ export default axios.create({
   //baseURL: "http://localhost:8091/", // auth backend on port 8091
   withCredentials: true, // important for Sanctum
   withXSRFToken: true,
+  credentials: "include" 
 });
