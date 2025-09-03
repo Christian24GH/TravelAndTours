d7e4e113099af7b9d8dc1d35fd1c3e81ee06c05d
Christian24GH <loquezchristian@gmail.com>
2025-09-04 00:51:46 +0800

    fixed token issue

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -4,4 +4,5 @@ export default axios.create({
   //baseURL: "https://auth-backend-v20b.onrender.com",
   baseURL: "http://localhost:8091", // auth backend on port 8091
   withCredentials: true, // important for Sanctum
+  withXSRFToken: true,
 });
