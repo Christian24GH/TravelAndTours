176f62038ac71def6e0d9f93d7a6a4faf82e5f25
Christian24GH <loquezchristian@gmail.com>
2025-09-04 16:51:42 +0800

    placeholders

--- frontend/src/main/landing.jsx
+++ frontend/src/main/landing.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
-
+import { Link } from 'react-router';
 export default function LandingPage() {
     return(
         <>
         Landing
+        Go to <Link to="/login" className="underline">Login</Link>
         </>
     )
 }
